00715a6555303c1251f5c636ac1280a05d7e1b26
Sasha <646415@student.inholland.nl>
2023-06-07 20:37:31 +0200

    fixed navbar

--- src/components/UserNavComponent.vue
+++ src/components/UserNavComponent.vue
@@ -8,12 +8,15 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
+          <!-- fir customer -->
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" :href="'/user/' + $route.params.id">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" :href="'/user/' + $route.params.id + '/atm'">ATM</a>
-            <!-- hardcoded for now-->
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/accounts/getIbanByCustomerName">Find accounts/Start transaction</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
@@ -26,10 +29,8 @@
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/accounts/getIbanByCustomerName">Find accounts</a>
-          </li>
           <li><a class="nav-link" @click="logout">Logout</a></li>
+
           <!-- for employee -->
           <li class="nav-item">
             <a class="nav-link" href="/users">User Management</a>
@@ -43,12 +44,11 @@
               Account Management
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" :href="'/accounts/'">Get all accounts</a></li>
-              <li><a class="dropdown-item" :href="'/accountsCreate/'">Create accounts</a></li>
-              <li><a class="dropdown-item" :href="'/accounts/accountStatus/iban'">Update accounts</a></li>
+              <li><a class="dropdown-item" :href="'/accounts'">Get all accounts</a></li>
+              <li><a class="dropdown-item" :href="'/accountsCreate'">Create accounts</a></li>
+              <li><a class="dropdown-item" :href="'/accounts/accountStatus'">Update accounts</a></li>
             </ul>
           </li>
-
         </ul>
       </div>
     </div>

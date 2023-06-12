ab3bbf647889f2b4f1482c2680cb5d0d2cad98cb
albaPlaceres <90030621+albaPlaceres@users.noreply.github.com>
2023-06-12 23:48:57 +0200

    transactions working

--- src/components/UserTransactionsComponent.vue
+++ src/components/UserTransactionsComponent.vue
@@ -2,17 +2,17 @@
   <section class="d-flex justify-content-center align-items-middle h-100">
     <form @submit.prevent="commitTransfer" class="card p-3">
       <h1>Create a transaction</h1>
-      <label>Account</label>
-      <select v-model="selectedAccount">
-        <option v-for="account in accounts" :key="account.id" :value="account.iban">
-          {{ account.iban }} ({{account.accountType}})
-        </option>
+      <label>Account from</label>
+      <select @change="changeSelectedAccount">
+        <option value="1">{{ accounts[0]?.iban }} ({{accounts[0]?.accountType}})</option>
+        <option value="2" v-if="accounts.length > 1">{{ accounts[1]?.iban }} ({{accounts[1]?.accountType}})</option>
       </select>
-      <span>Balance: &euro; {{selectedAccountBalance.toFixed(2)}}</span>
       <label class="my-2">Amount <small>*required</small></label>
-      <input type="number" step="0.01" min="0" required />
+      <input type="number" step="0.01" min="0" v-model="amount" required />
       <label class="my-2">Account to</label>
       <input type="text" v-model="targetIBAN" disabled />
+      <label for="description">Description:</label>
+      <input type="text" name="desciption" v-model="description">
       <button class="btn btn-primary my-3">Transfer</button>
       <div v-if="errorHappened" class="alert alert-danger">{{error}}</div>
       <div v-if="successHappened" class="alert alert-success">{{success}}</div>
@@ -35,6 +35,8 @@ export default {
       errorHappened: false,
       success: '',
       successHappened: false,
+      amount: 0,
+      description: "",
     }
   },
   mounted() {
@@ -49,6 +51,9 @@ export default {
     async GetAccounts() {
       await axios.get('/accounts/myAccounts/' + this.userId).then((response) => {
         this.accounts = response.data
+        if (this.accounts.length > 0){
+          this.selectedAccount = this.accounts[0]
+        }
       })
     },
     commitTransfer() {
@@ -59,16 +64,25 @@ export default {
       axios.post('/transactions', {
         fromIban: this.selectedAccount.iban,
         toIban: this.targetIBAN,
-        amount: this.amount
+        amount: this.amount,
+        description: this.description,
       }).then((response) => {
         this.success = "Transfer successful!"
         this.successHappened = true
         // Get accounts again to update the balance
         this.GetAccounts();
       }).catch((error) => {
-        this.error = error.response.data.message
+        this.error = error.response.data.error
         this.errorHappened = true
       })
+    },
+    changeSelectedAccount(value) {
+      if (value.target.value == 1){
+        this.selectedAccount = this.accounts[0];
+      } 
+      else if (value.target.value == 2){
+        this.selectedAccount = this.accounts[1];
+      }
     }
   },
   computed: {

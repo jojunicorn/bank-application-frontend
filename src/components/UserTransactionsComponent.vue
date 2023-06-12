ef7ff0d64ca6480cf0d3abc80fbbf4fbbccfc24a
albaPlaceres <90030621+albaPlaceres@users.noreply.github.com>
2023-06-12 21:24:06 +0200

    perform transaction changes

--- src/components/UserTransactionsComponent.vue
+++ src/components/UserTransactionsComponent.vue
@@ -1,36 +1,49 @@
 <template>
   <section class="d-flex justify-content-center align-items-middle h-100">
-      <form @submit.prevent class="card p-3">
-        <h1>Create a transaction</h1>
-        <label>Account</label>
-        <select>
-          <option value="CURRENT">put current</option>
-          <option value="SAVING">put saviing</option>
-        </select>
-        <label>Amount <small>*required</small></label>
-        <input type="number" step="0.01" min="0" required>
-        <label>Account to</label>
-        <input type="text" v-model="targetIBAN" disabled/>
-      </form>
+    <form @submit.prevent class="card p-3">
+      <h1>Create a transaction</h1>
+      <label>Account</label>
+      <select v-model="selectedAccount">
+        <option v-for="account in accounts" :key="account.id" :value="account.iban">
+          {{ account.iban }}
+        </option>
+      </select>
+      <label>Amount <small>*required</small></label>
+      <input type="number" step="0.01" min="0" required />
+      <label>Account to</label>
+      <input type="text" v-model="targetIBAN" disabled />
+    </form>
   </section>
 </template>
 
 <script>
+import axios from '../axiosConfig'
+
 export default {
-  name: "UserTransactionsComponent",
+  name: 'UserTransactionsComponent',
   data() {
     return {
+      accounts: [],
+      userId: null,
       targetIBAN: ''
     }
   },
   mounted() {
-  if (this.$eventBus.tempIban != null)
-  {
-    this.targetIBAN = this.$eventBus.tempIban;
+    if (this.$eventBus.tempIban != null) {
+      this.targetIBAN = this.$eventBus.tempIban
+    }
+
+    this.userId = localStorage.getItem('id')
+    this.GetAccounts()
+  },
+  methods: {
+    async GetAccounts() {
+      await axios.get('/accounts/myAccounts/' + this.userId).then((response) => {
+        this.accounts = response.data
+      })
+    }
   }
 }
-}
-
 </script>
 
 <style>
